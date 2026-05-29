e5091f77466ae4a54d67e9714b42956d7559d798
dqwangyang <yangwang_hn@163.com>
2026-05-29 15:54:06 +0800

    fix: correct GitHub repo URL from astock-insight to dqwangyang

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="astock-insight",
-    url="https://github.com/astock-insight/astock-insight",
+    url="https://github.com/dqwangyang/astock-insight",
     packages=find_packages(),
     python_requires=">=3.8",
     entry_points={
